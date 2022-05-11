1cfd01196f60340c24aaabec507638e0fbef1ee6
Juliana Araujo <julianaraujo555@outlook.com>
2022-05-10 22:05:43 -0300

    fixing schedule

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,31 +35,6 @@ const Home = () => {
           <span>Gratuitamente no youtube, ao vivo</span>
         </div>
       </div>
-      {/* <div className={`${styles.wrapper} container`}>
-        <div className={`${styles.main}`}>
-          <h1 className="title">Destralhe</h1>
-          <h1 className="title">seu Instagram Jurídico</h1>
-          <p>
-            Elimine todos os erros e incertezas que você vem acumulando no seu
-            Instagram Jurídico e aprenda estratégias simples que funcionam para
-            prospectar clientes.
-          </p>
-
-          <span>
-            {" "}
-            <BsCalendar color="#254528" />
-            Dias <strong>23, 24 e 25 de maio</strong>, às 20h27
-          </span>
-        </div>
-        <a
-          href="https://chat.whatsapp.com/L62ACsNRcTYHoEzSHQMZ1r"
-          target="_blank"
-          className={styles.button}
-        >
-          Quero Participar
-        </a>
-        <span>Gratuitamente no youtube, ao vivo</span>
-  </div>*/}
     </header>
   );
 };
